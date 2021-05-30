9b8e5c207e985a9357f82461568ce246f40b8daf
awashbrook <andy.washbrook@thomsonreuters.com>
2021-05-30 10:46:37 +0100

    Updating instance ids much longer now...

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,9 +1,5 @@
-variable "AWS_ACCESS_KEY" {
-}
-
-variable "AWS_SECRET_KEY" {
-}
-
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
@@ -11,9 +7,8 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-0ee02acd56a52998e"
+    us-west-2 = "ami-0dd273d94ed0540c0"
+    eu-west-1 = "ami-038d7b856fe7557b3"
   }
 }
-
