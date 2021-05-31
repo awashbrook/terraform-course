5e14ddaff084462db5a042e061103958455930d0
awashbrook <awashbrook@gmail.com>
2021-05-31 14:46:09 +0100

    Synced prior updates...

--- demo-5/vars.tf
+++ demo-5/vars.tf
@@ -1,3 +1,5 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
@@ -5,9 +7,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-0ee02acd56a52998e"
+    us-west-2 = "ami-0dd273d94ed0540c0"
+    eu-west-1 = "ami-038d7b856fe7557b3"
   }
 }
 
