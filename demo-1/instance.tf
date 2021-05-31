98251cf67f20ce29bc4937f8b17791ab03c4c2a0
awashbrook <awashbrook@gmail.com>
2021-05-31 11:03:19 +0100

    AWS key pair

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,5 +1,19 @@
+resource "aws_key_pair" "andy-wash" {
+  key_name = "mykey"
+  public_key = "ssh-rsa my-public-key"
+}
+
 resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.mykey.key_name}"
 }
 
+provisioner "file" {
+  source = "script.sh"
+  destination = "/opt/script.sh"
+  connection {
+    user = var.instance_username
+    password = ${file(${var.path_to_private_key})}
+  }
+}
