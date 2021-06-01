c1ee9c4feb6927cc5274ddc8298f9bba72586e6e
awashbrook <awashbrook@gmail.com>
2021-06-01 10:50:44 +0100

    AWS keys and key pair for Consul demo...

--- demo-6/vars.tf
+++ demo-6/vars.tf
@@ -1,12 +1,31 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
 
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0ee02acd56a52998e"
+    us-west-2 = "ami-0dd273d94ed0540c0"
+    eu-west-1 = "ami-038d7b856fe7557b3"
+  }
+}
+
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "~/.ssh/mykey"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "~/.ssh/mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
 }
 
