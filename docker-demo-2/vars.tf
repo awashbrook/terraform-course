bae532d30953e358fa9d8e37e40173ee3460e652
awashbrook <andy.washbrook@thomsonreuters.com>
2021-07-06 20:26:47 +0100

    Syncing changes from other demos

--- docker-demo-2/vars.tf
+++ docker-demo-2/vars.tf
@@ -1,13 +1,14 @@
+variable "WORKSTATION_CIDR_BLOCK" {
+}
+
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
 variable "AWS_REGION" {
   default = "eu-west-1"
-}
-
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
 }
 
 variable "ECS_INSTANCE_TYPE" {
@@ -17,10 +18,20 @@ variable "ECS_INSTANCE_TYPE" {
 variable "ECS_AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-1924770e"
-    us-west-2 = "ami-56ed4936"
-    eu-west-1 = "ami-c8337dbb"
+    us-east-1 = "ami-0ee02acd56a52998e"
+    us-west-2 = "ami-0dd273d94ed0540c0"
+    eu-west-1 = "ami-038d7b856fe7557b3"
   }
 }
 
-# Full List: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
